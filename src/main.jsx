c5cb192b4d4822051a3238759d24810c40ad7a11
abinandhan1808 <abinandhan@my-lawyer.in>
2026-05-08 16:20:40 +0530

    add reload

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,14 @@ import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.jsx'
 
+const oneTimeReloadKey = 'mlawyer:reloaded-once'
+const hasReloadedOnce = sessionStorage.getItem(oneTimeReloadKey) === 'true'
+
+if (!hasReloadedOnce) {
+  sessionStorage.setItem(oneTimeReloadKey, 'true')
+  window.location.reload()
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
